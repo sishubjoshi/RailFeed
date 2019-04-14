e15c145f23480bc4dd1d096fb503b5cc59a2c60b
sishubjoshi <shisub1999@gmail.com>
2019-04-14 09:39:42 +0530

    create-react-app initialized and created Nav

---
+++ myapp/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar bg-dark">
+            <h1 className="text-warning">Railway Complaint</h1>
+            <div className="underline"></div>      
+      </nav>
+    </div>
+  )
+}
+
+export default Header;
